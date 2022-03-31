ba14cc2e6a0ebb7b0d8a638724254fac3aa94a5c
holawan <asdf134652@gmail.com>
2022-03-31 22:54:28 +0900

    base_html만들고 경로 설정해주기

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 
 # 브라우저에서 경로로 접속할 때, Hello world를 출력하는 view 만들기
+#HTML을 가져오는게 아니라 직접 응답을 만들어서 되돌려주는 형태
+# def hello_world(request) :
+#     return HttpResponse('Hello world!')
+
 def hello_world(request) :
-    return HttpResponse('Hello world!')
+    return render(request,'base.html')
 
