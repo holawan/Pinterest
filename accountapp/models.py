608f25f22259125cd80f94dbbe0b04ceb92f887b
holawan <asdf134652@gmail.com>
2022-04-04 20:04:17 +0900

    DB생성하고 migration

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# models에서 Model(기초적인 모델)을 상속 받음 
+class HelloWorld(models.Model) :
+
+    text = models.CharField(max_length=255,null = False)
