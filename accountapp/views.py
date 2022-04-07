946e98b7a0373e83df4a16c0f12875bcae3e073c
holawan <asdf134652@gmail.com>
2022-04-07 12:51:27 +0900

    로그인 하지 않으면 메인페이지 접속 시 로그인 창으로 이동

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,4 +1,5 @@
 # from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView,DetailView,UpdateView,DeleteView
@@ -15,26 +16,34 @@ from accountapp.models import HelloWorld
 #     return HttpResponse('Hello world!')
 
 def hello_world(request) :
-    # get 요청을 받으면 get METHOD를 표시 
-    if request.method == "POST" : 
-        # request 의 post 중에서 hello_world_input 데이터를 가져오고 tmp에 넣어라
-        temp = request.POST.get('hello_world_input')
-        # DB를 구성한 모델을 가져온다. 
+
+    #사용자가 로그인 안했으면 로그인 창으로 보내기
+
+    # user가 로그인 했으면 
+    if request.user.is_authenticated :
+        # get 요청을 받으면 get METHOD를 표시 
+        if request.method == "POST" : 
+            # request 의 post 중에서 hello_world_input 데이터를 가져오고 tmp에 넣어라
+            temp = request.POST.get('hello_world_input')
+            # DB를 구성한 모델을 가져온다. 
 
 
-        new_hello_world = HelloWorld()
-        #POST로 받아온 값을 DB에 추가해준다.
-        #new_hello_world 인스턴스의 text에 temp를 저장 
-        new_hello_world.text = temp
-        new_hello_world.save()
+            new_hello_world = HelloWorld()
+            #POST로 받아온 값을 DB에 추가해준다.
+            #new_hello_world 인스턴스의 text에 temp를 저장 
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        # 경로를 다시 만들어주려면 reverse라는 함수를 써야한다. 
-        #어카운트에 헬로우 월드로 재접속하라 ?
-        return redirect(reverse('accountapp:hello_world'))
-    # POST 요청을 받으면 POST MEHTOD 표시 
+            # 경로를 다시 만들어주려면 reverse라는 함수를 써야한다. 
+            #어카운트에 헬로우 월드로 재접속하라 ?
+            return redirect(reverse('accountapp:hello_world'))
+        # POST 요청을 받으면 POST MEHTOD 표시 
+        else :
+            hello_world_list = HelloWorld.objects.all()[::-1]
+            return render(request,'accountapp/hello_world.html',context={'hello_world_list': hello_world_list})
+    # 로그인 되어있지 않으면 로그인 창으로 자동으로 보내기 
     else :
-        hello_world_list = HelloWorld.objects.all()[::-1]
-        return render(request,'accountapp/hello_world.html',context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 # 장고의 크리에이트 뷰 상속 받기 
 class AccountCreateView(CreateView) :
