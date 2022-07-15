341810f12a08b70ac7a1365d27a3ef54f05c149b
asdf134652 <asdf134652@gmail.com>
2022-07-15 20:47:10 +0900

    Docs: enviornment 위치 이동

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -14,18 +14,24 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 
-import environ
+import environ,os
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 
+#1 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # 베이스 디렉토리는 settings.py 경로에서 root폴더로 가서 그 폴더를 BASE_DIR로 하겠다.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # reading .env file
-environ.Env.read_env()
+# environ.Env.read_env()
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+print(BASE_DIR)
+# print(env('SECRET_KEY'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
