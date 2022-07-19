5321eef1b961d1822ff729416f5466cf39404fbe
asdf134652 <asdf134652@gmail.com>
2022-07-19 19:17:00 +0900

    Refactor: deploy  경로 수정

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -25,7 +25,9 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
